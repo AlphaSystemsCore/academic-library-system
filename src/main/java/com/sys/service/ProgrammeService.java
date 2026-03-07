bd12366d0fbfaffa6971f66413ba81539db5707d
alphonce-tech <alphonceodongo005@gmail.com>
2026-03-07 22:13:23 +0300

    left at prog dao

--- src/main/java/com/sys/service/ProgrammeService.java
+++ src/main/java/com/sys/service/ProgrammeService.java
@@ -1,7 +1,11 @@
 package com.sys.service;
 
+import com.sys.model.Programme.LevelEnum;
+
 public class ProgrammeService {
-    public void addProgrammeService(String ){};
+    public void addProgrammeService(int programmeId, String name, String programmeCode, LevelEnum level, int durationYears, int departmentId){
+
+    }
     public void viewProgrammeService(){}
     public void deleteProgrammeService(){}
     public void updateProgrammeAllService(){}
